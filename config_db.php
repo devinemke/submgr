40a3e186cb1d9bd067ed00118f39f8fbd93d03fc
devinemke <devinemke@gmail.com>
2021-03-14 08:47:40 -0400

    add config_db.php template

---
+++ config_db.php
@@ -0,0 +1,11 @@
+<?php
+$config_db = array(
+'host' => '{host}',
+'username' => '{username}',
+'password' => '{password}',
+'name' => '{name}',
+'port' => '{port}'
+);
+
+define('INSTALLED', false);
+?>
